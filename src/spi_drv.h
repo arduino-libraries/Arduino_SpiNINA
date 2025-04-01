7ae58f513b2008a1890a4f28403a2b92a6389381
pennam <m.pennasilico@arduino.cc>
2025-04-01 15:58:35 +0200

    clang format

--- src/spi_drv.h
+++ src/spi_drv.h
@@ -14,77 +14,52 @@
 #include <inttypes.h>
 #include "spi_types.h"
 
-#define SPI_START_CMD_DELAY 	10
+#define SPI_START_CMD_DELAY 10
 
-#define NO_LAST_PARAM   0
-#define LAST_PARAM      1
+#define NO_LAST_PARAM 0
+#define LAST_PARAM 1
 
-#define DUMMY_DATA  0xFF
+#define DUMMY_DATA 0xFF
 
-#define WAIT_FOR_SLAVE_SELECT()	      \
-	if (!SpiDrv::initialized) {           \
-		SpiDrv::begin();      \
-	}                             \
-	SpiDrv::waitForSlaveReady();  \
-	SpiDrv::spiSlaveSelect();
+#define WAIT_FOR_SLAVE_SELECT() \
+  if (!SpiDrv::initialized) { \
+    SpiDrv::begin(); \
+  } \
+  SpiDrv::waitForSlaveReady(); \
+  SpiDrv::spiSlaveSelect();
 
-class SpiDrv
-{
+class SpiDrv {
 private:
-	static void waitForSlaveSign();
-	static void getParam(uint8_t* param);
+  static void waitForSlaveSign();
+  static void getParam(uint8_t* param);
 public:
-    static bool initialized;
+  static bool initialized;
 
-    static void begin(bool force=false);
-
-    static void end();
-
-    static void spiDriverInit();
-
-    static void spiSlaveSelect();
-
-    static void spiSlaveDeselect();
-
-    static char spiTransfer(volatile char data);
-
-    static void waitForSlaveReady(bool const feed_watchdog = false);
-
-    static int waitSpiChar(unsigned char waitChar);
-
-    static int readAndCheckChar(char checkChar, char* readChar);
-
-    static char readChar();
-
-    static int waitResponseParams(uint8_t cmd, uint8_t numParam, tParam* params);
-
-    static int waitResponseCmd(uint8_t cmd, uint8_t numParam, uint8_t* param, uint8_t* param_len);
-
-    static int waitResponseData8(uint8_t cmd, uint8_t* param, uint8_t* param_len);
-
-    static int waitResponseData16(uint8_t cmd, uint8_t* param, uint16_t* param_len);
-
-    static int waitResponse(uint8_t cmd, uint8_t* numParamRead, uint8_t** params, uint8_t maxNumParams);
-
-    static void sendParam(const uint8_t* param, uint8_t param_len, uint8_t lastParam = NO_LAST_PARAM);
-
-    static void sendParamNoLen(const uint8_t* param, size_t param_len, uint8_t lastParam = NO_LAST_PARAM);
-
-    static void sendParamLen8(const uint8_t param_len);
-
-    static void sendParamLen16(const uint16_t param_len);
-
-    static uint8_t readParamLen8(uint8_t* param_len = NULL);
-
-    static uint16_t readParamLen16(uint16_t* param_len = NULL);
-
-    static void sendBuffer(const uint8_t* param, uint16_t param_len, uint8_t lastParam = NO_LAST_PARAM);
-
-    static void sendParam(uint16_t param, uint8_t lastParam = NO_LAST_PARAM);
-
-    static void sendCmd(uint8_t cmd, uint8_t numParam);
-
-    static int available();
+  static void begin(bool force = false);
+  static void end();
+  static void spiDriverInit();
+  static void spiSlaveSelect();
+  static void spiSlaveDeselect();
+  static char spiTransfer(volatile char data);
+  static void waitForSlaveReady(bool const feed_watchdog = false);
+  static int waitSpiChar(unsigned char waitChar);
+  static int readAndCheckChar(char checkChar, char* readChar);
+  static char readChar();
+  static int waitResponseParams(uint8_t cmd, uint8_t numParam, tParam* params);
+  static int waitResponseCmd(uint8_t cmd, uint8_t numParam, uint8_t* param, uint8_t* param_len);
+  static int waitResponseData8(uint8_t cmd, uint8_t* param, uint8_t* param_len);
+  static int waitResponseData16(uint8_t cmd, uint8_t* param, uint16_t* param_len);
+  static int waitResponse(uint8_t cmd, uint8_t* numParamRead, uint8_t** params, uint8_t maxNumParams);
+  static void sendParam(const uint8_t* param, uint8_t param_len, uint8_t lastParam = NO_LAST_PARAM);
+  static void sendParamNoLen(const uint8_t* param, size_t param_len, uint8_t lastParam = NO_LAST_PARAM);
+  static void sendParamLen8(const uint8_t param_len);
+  static void sendParamLen16(const uint16_t param_len);
+  static uint8_t readParamLen8(uint8_t* param_len = NULL);
+  static uint16_t readParamLen16(uint16_t* param_len = NULL);
+  static void sendBuffer(const uint8_t* param, uint16_t param_len, uint8_t lastParam = NO_LAST_PARAM);
+  static void sendParam(uint16_t param, uint8_t lastParam = NO_LAST_PARAM);
+  static void sendCmd(uint8_t cmd, uint8_t numParam);
+  static int available();
 };
 
 extern SpiDrv spiDrv;
