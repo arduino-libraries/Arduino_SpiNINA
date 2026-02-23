42989c931a5a7ad141908ec9b1cbf8d654480583
pennam <m.pennasilico@arduino.cc>
2026-02-23 10:45:37 +0100

    library: update copyright

--- src/spi_drv.h
+++ src/spi_drv.h
@@ -1,7 +1,7 @@
 /*
   This file is part of the Arduino_SpiNINA library.
 
-  Copyright (c) 2025 Arduino SA
+  Copyright (C) Arduino s.r.l. and/or its affiliated companies
 
   This Source Code Form is subject to the terms of the Mozilla Public
   License, v. 2.0. If a copy of the MPL was not distributed with this
